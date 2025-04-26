bfc0a240054518942db7a16cd182b0004d28f712
xordi <jordi.obuades@gmail.com>
2025-04-26 17:15:43 +0200

    Add codecompletion with copilot

---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,18 @@
+return {
+  "olimorris/codecompanion.nvim",
+  opts = {
+    strategies = {
+      chat = {
+        adapter = "copilot",
+      },
+      inline = {
+        adapter = "copilot",
+      },
+    }
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = true,
+}
