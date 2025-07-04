4e01b337caf3918430232c63cd508ecbc71a8422
xordi <jordi.buades@helloprima.com>
2025-07-04 10:11:59 +0200

    Use claude 3.7 for inline & cmd

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -4,9 +4,15 @@ return {
     strategies = {
       chat = {
         adapter = "copilot",
+        model = "gpt-4.1",
       },
       inline = {
         adapter = "copilot",
+        model = "claude-3-7-sonnet-20250219"
+      },
+      cmd = {
+        adapter = "copilot",
+        model = "claude-3-7-sonnet-20250219"
       },
     },
     display = {
